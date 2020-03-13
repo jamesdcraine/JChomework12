8fffb30b9ba3fa6c83e6a2870a16a5f4815ea3c2
James Craine <jamesdcraine@gmail.com>
2020-03-12 20:59:40 -0400

    finishing up for the night

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -8,12 +8,14 @@ const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
 
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
 
 fs
   .readdirSync(__dirname)
@@ -25,11 +27,15 @@ fs
     db[model.name] = model;
   });
 
+
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
+
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
